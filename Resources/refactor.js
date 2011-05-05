5ea2071f1f1e9cc15233f41177566c1ee297463d
Randy Weinstein <sigmonky@gmail.com>
2011-05-05 14:28:16 -0400

    refactored

--- Resources/refactor.js
+++ Resources/refactor.js
@@ -2,20 +2,32 @@
  * @author Weinstein, Randy 
  */
 
-var HSHED = {
-	
-	var band = {
-		var name,
-		        players  = [];
-	} ;
-	band.addPlayer =function(name) {
-		
-	}
-	var song = {}
-	var library = {}
-	var players = {};
-	players.makePlayer = function( name,instrument ) {
-		return {"name":name,"instrument":instrument};
-	}
+var Sound = function() {
+	return {};
 };
+
+var Instrument = function() {
+	var soundBank = {};
+	return {};
+};
+
+var Player = function() {
+	var instrument;
+};
+
+var Band = function() {
+	var players = {};
+	var songBook;
+	return {};
+};
+
+var Song = function() {
+	return {};
+};
+
+var Songbook = function() {
+	return {};
+};
+
+
 
